15e61619fe500f6607a2f0c58699ea674b7779af
gushengjie <guchee@163.com>
2024-05-12 22:28:02 +0800

    feat: Add encode.hh and encode.cpp files
    This commit adds the encode.hh and encode.cpp files to the project. These files contain functions for encoding and decoding data in the LSM tree. The functions include Decode32, Encode32, EncodeWithPreLen, DecodeWithPreLen, and Decode64. These functions are essential for handling data in the LSM tree.
    
    Refs: #123

---
+++ include/encode.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+namespace lsm_tree {
+using std::string;
+using std::string_view;
+void Decode32(const char *src, int *dest);
+void Encode32(int src, char *dest);
+void EncodeWithPreLen(string &dest, string_view data);
+auto DecodeWithPreLen(string &dest, string_view data) -> int;
+void Decode64(const char *src, int64_t *dest);
+
+}  // namespace lsm_tree
---
+++ source/encode.cpp
@@ -0,0 +1,58 @@
+#include "encode.hh"
+#include <cstring>
+#include <string>
+
+namespace lsm_tree {
+
+/**
+ * @brief 将src中的前4个字节解析为int
+ *
+ * @param src
+ * @param dest
+ */
+void Decode32(const char *src, int *dest) { *dest = *reinterpret_cast<const int *>(src); }
+
+/**
+ * @brief 将src编码为4字节的char数组
+ *
+ * @param src
+ * @param dest
+ */
+void Encode32(int src, char *dest) { memcpy(dest, &src, sizeof(int)); }
+
+/**
+ * @brief 将data编码为带有长度前缀的字符串
+ *
+ * @param dest
+ * @param data
+ */
+void EncodeWithPreLen(string &dest, string_view data) {
+  int len = data.size();
+  dest.append(reinterpret_cast<const char *>(&len), sizeof(int));
+  dest.append(data.data(), len);
+}
+
+/**
+ * @brief 解析带有长度前缀的字符串
+ *
+ * @param dest
+ * @param data
+ * @return int
+ */
+auto DecodeWithPreLen(string &dest, string_view data) -> int {
+  int         len;
+  const char *buf = data.data();
+  Decode32(buf, &len);
+  dest.assign(buf + sizeof(int), len);
+  return len + sizeof(int);
+}
+
+/**
+ * @brief 将src中的前8个字节解析为int64_t
+ *
+ * @param src
+ * @param dest
+ */
+void Decode64(const char *src, int64_t *dest) { *dest = *(int64_t *)(src); }
+
+}  // namespace lsm_tree
