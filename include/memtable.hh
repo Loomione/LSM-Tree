631bef8da24386bdbb01c5779da71bf5aa9d26e6
gushengjie <guchee@163.com>
2024-05-16 23:31:31 +0800

    feat: Update MemTable class with shared mutex for thread-safe access
    The MemTable class in the memtable.hh file has been updated to use a shared mutex instead of a regular mutex for thread-safe access. This change allows multiple threads to read from the MemTable simultaneously while still ensuring exclusive access during write operations.
    
    Refs: #123

--- include/memtable.hh
+++ include/memtable.hh
@@ -1,15 +1,27 @@
 #pragma once
+
 #include <map>
 #include <memory>
-#include <mutex>
+#include <shared_mutex>
 #include <string>
+#include "keys.hh"
 #include "wal.hh"
+
 namespace lsm_tree {
 
 struct DBOptions;
 class MemTable {
  public:
-  struct Stat {};
+  struct Stat {
+    Stat() = default;
+    void Update(size_t key_size, size_t value_size) {
+      keys_size_ += key_size;
+      values_size_ += value_size;
+    }
+    auto   Sum() -> size_t { return keys_size_ + values_size_; }
+    size_t keys_size_{0};
+    size_t values_size_{0};
+  };
   explicit MemTable(const DBOptions &options);
   MemTable(const DBOptions &options, WAL *wal);
   MemTable(const MemTable &)                   = delete;
@@ -18,21 +30,22 @@ class MemTable {
   ~MemTable() = default;
 
  public:
-  auto Put(const MemKey &key, string_view value) -> RC;
-  auto PutTeeWAL(const MemKey &key, string_view value) -> RC;
-  auto Get(string_view key, string &value, int64_t seq = INT64_MAX) -> RC;
-  auto GetNoLock(string_view key, string &value, int64_t seq = INT64_MAX) -> RC;
-  auto ForEachNoLock(std::function<RC(const MemKey &key, string_view value)> &&func) -> RC;
-  auto BuildSSTable(string_view dbname, FileMetaData **meta_data_pointer) -> RC;
-  auto GetMemTableSize() -> size_t;
-  auto DropWAL() -> RC;
-  auto Empty() -> bool;
+  auto Empty() -> bool;                                                           // 判断MemTable是否为空
+  auto Put(const MemKey &key, string_view value) -> RC;                           // 插入数据
+  auto PutTeeWAL(const MemKey &key, string_view value) -> RC;                     // 插入数据, 并写入WAL
+  auto GetMemTableSize() -> size_t;                                               // 获取MemTable的大小
+  auto DropWAL() -> RC;                                                           // 删除WAL
+  auto Get(string_view key, string &value, int64_t seq = INT64_MAX) -> RC;        // 查询数据
+  auto GetNoLock(string_view key, string &value, int64_t seq = INT64_MAX) -> RC;  // 查询数据, 不加锁
+  auto BuildSSTable(string_view dbname, FileMetaData **meta_data_pointer) -> RC;  // 构建SSTable
+  auto ForEachNoLock(std::function<RC(const MemKey &key, string_view value)> &&func) -> RC;  // 遍历数据, 不加锁
 
  private:
-  Stat                 stat_;
-  std::unique_ptr<WAL> wal_;
-  mutable std::mutex   mtx_;
-
-  std::map<std::string, std::string> table_;
+  Stat                          stat_;
+  std::unique_ptr<WAL>          wal_;
+  mutable std::shared_mutex     mtx_;
+  const DBOptions              *options_;
+  std::map<MemKey, std::string> table_;
 };
+
 }  // namespace lsm_tree
---
+++ source/memtable.cpp
@@ -0,0 +1,111 @@
+/**
+ * @file memtable.cpp
+ * @author gusj (guchee@163.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-05-16
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "memtable.hh"
+#include <mutex>
+#include <shared_mutex>
+#include "keys.hh"
+#include "util/monitor_logger.hh"
+
+namespace lsm_tree {
+
+MemTable::MemTable(const DBOptions &options) : options_(&options) {}
+MemTable::MemTable(const DBOptions &options, WAL *wal) : options_(&options) { wal_.reset(wal); }
+
+auto MemTable::Empty() -> bool {
+  std::shared_lock lock(mtx_);
+  return table_.empty();
+}
+
+auto MemTable::Put(const MemKey &key, string_view value) -> RC {
+  std::unique_lock lock(mtx_);
+  if (key.type_ == OperatorType::PUT) {
+    table_[key] = value;
+  } else {
+    table_[key] = "";
+  }
+  stat_.Update(key.Size(), value.size());
+  return RC::OK;
+}
+
+auto MemTable::PutTeeWAL(const MemKey &key, string_view value) -> RC {
+  auto rc = RC::OK;
+  if (rc = wal_->AddRecord(EncodeKVPair(key, value)); rc != RC::OK) {
+    return rc;
+  }
+  if (options_->sync_) {
+    if (rc = wal_->Sync(); rc != RC::OK) {
+      return rc;
+    }
+  }
+  return Put(key, value);
+}
+
+/* 暂时用不加锁版本 */
+auto MemTable::ForEachNoLock(std::function<RC(const MemKey &key, string_view value)> &&func) -> RC {
+  for (auto &iter : table_) {
+    const MemKey &memkey = iter.first;
+    string_view   value  = iter.second;
+    auto          rc     = func(memkey, value);
+    if (rc != RC::OK) {
+      return rc;
+    }
+  }
+  return RC::OK;
+}
+
+/* 一般是 IMEMTABLE 进行 BUILD 不需要加锁 */
+auto MemTable::BuildSSTable(string_view dbname, FileMetaData **meta_data_pointer) -> RC {
+    // TODO (gsj)
+}
+
+auto MemTable::GetMemTableSize() -> size_t {
+  std::shared_lock lock(mtx_);
+  return stat_.Sum();
+}
+
+auto MemTable::DropWAL() -> RC {
+  auto rc = RC::OK;
+  if (wal_) {
+    MLog->trace("MemTable::DropWAL");
+    if (rc = wal_->Sync(); rc != RC::OK) {
+      return rc;
+    }
+    if (rc = wal_->Close(); rc != RC::OK) {
+      return rc;
+    }
+    if (rc = wal_->Drop(); rc != RC::OK) {
+      return rc;
+    }
+    wal_.reset();
+  }
+  return rc;
+}
+
+auto MemTable::Get(string_view key, string &value, int64_t seq) -> RC {
+  std::unique_lock lock(mtx_);
+  return GetNoLock(key, value);
+}
+
+auto MemTable::GetNoLock(string_view key, string &value, int64_t seq) -> RC {
+  MemKey look_key(key, seq);
+  auto   iter = table_.lower_bound(look_key);
+  if (iter == table_.end()) {
+    return RC::NOT_FOUND;
+  }
+  if (key == iter->first.user_key_ && iter->first.type_ != OperatorType::DELETE) {
+    value = iter->second;
+    return RC::OK;
+  }
+  return RC::NOT_FOUND;
+}
+
+}  // namespace lsm_tree
