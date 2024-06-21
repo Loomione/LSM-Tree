8c9870a76a949eb99e1a59c5011450cced2d9d63
gushengjie <guchee@163.com>
2024-06-21 17:20:15 +0800

    refactor: 移除不再需要的third_party/crc32c/include路径
    这个提交移除了CMakeLists.txt文件中的third_party/crc32c/include路径，因为它不再需要。这个更改提高了代码的可读性和维护性。

--- readme.md
+++ readme.md
@@ -5,3 +5,13 @@
 
 在`test`目录下，添加了单元测试代码，使用`googletest`框架进行测试。
 编译时，需要在`build`目录下执行`cmake .`，然后`make + 测试文件名`进行编译。
+
+## 依赖
+1. crc32c ,需要安装到 /usr目录
+    ```bash
+    cmake -DCMAKE_INSTALL_PREFIX=/usr -DCRC32C_BUILD_TESTS=0 -DCRC32C_BUILD_BENCHMARKS=0 ..
+    ```
+    ```bash
+    make all
+    sudo make install
+    ```
--- test/file_test/file_util_test.cpp
+++ test/file_test/file_util_test.cpp
@@ -1,8 +1,27 @@
-#include "gtest/gtest.h"
 #include "util/file_util.hh"
+#include "gtest/gtest.h"
 
 TEST(FileUtil, CreatFile) {
-  std::string path = "test/file_test/test_file";
-  auto        rc   = lsm_tree::file_manager::Create(path, lsm_tree::FileOptions::FILE_);
-  EXPECT_EQ(rc, lsm_tree::RC::OK);
+  std::string dir = "/home/swarm/spectrum_sensing/gsj/lsm/build/test/files";
+  {
+    auto rc = lsm_tree::file_manager::Create(dir, lsm_tree::FileOptions::DIR_);
+    EXPECT_EQ(rc, lsm_tree::RC::OK);
+  }
+  auto path = dir + "/test.txt";
+  {
+    auto rc = lsm_tree::file_manager::Create(path, lsm_tree::FileOptions::FILE_);
+    EXPECT_EQ(rc, lsm_tree::RC::OK);
+  }
+  {
+    auto rc = lsm_tree::file_manager::Exists(path);
+    EXPECT_EQ(rc, true);
+  }
+  {
+    auto rc = lsm_tree::file_manager::IsDirectory(path);
+    EXPECT_EQ(rc, false);
+  }
+  {
+    auto rc = lsm_tree::file_manager::IsDirectory(dir);
+    EXPECT_EQ(rc, true);
+  }
 }
